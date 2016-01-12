7071c76e7c467402b03544d6c5e202449899f526
gasolin <gasolin@gmail.com>
2016-01-12 12:04:57 +0000

    rebuild pages at 74a1758

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*globals $, defaultVerbStore, verbSearch, localforage, UrlHelper */
+/*globals $, defaultVerbStore, verbSearch, localforage, UrlHelper, DialogManager */
 'use strict';
 
 /**
@@ -262,64 +262,6 @@ var registerKeyboardHandlers = function() {
       break;
     }
   });
-};
-
-/**
- * Manage chat stream.
- */
-var chatKey = 'chatstore';
-var DialogManager = {
-  _chatSteam: [],
-
-  init: function() {
-    localforage.getItem(chatKey, function(err, value) {
-      if (err) {
-        console.error(err);
-      } else {
-        var dialogs = JSON.parse(value);
-        if (dialogs) {
-          this._chatSteam = dialogs;
-          this.renderAll();
-        }
-      }
-    }.bind(this));
-  },
-
-  push: function(item) {
-    if (item.hasOwnProperty('timestamp')) {
-      item.timestamp = Date.now();
-    }
-    this._chatSteam.push(item);
-    this.render(item);
-
-    // cache last 8 items
-    var len = this._chatSteam.length;
-    if(len > 8) {
-      this._chatSteam.splice(0, len - 8);
-    }
-    localforage.setItem(chatKey, JSON.stringify(this._chatSteam))
-      .then(function() {
-        // console.log('dialog cached');
-      });
-  },
-
-  render: function(item) {
-    var chatbox = document.createElement('section');
-    chatbox.classList.add('chat', item.speaker + '-chat');
-    //chatbox.textContent = "I am " + target.dataset.type + "ing \"" + decodeURI(target.dataset.key) + "\" with " + target.id;
-    chatbox.innerHTML = item.msg;
-    chatHistory.appendChild(chatbox);
-  },
-
-  renderAll: function() {
-    this._chatSteam.forEach(function(item) {
-      var chatbox = document.createElement('section');
-      chatbox.classList.add('chat', item.speaker + '-chat');
-      //chatbox.textContent = "I am " + target.dataset.type + "ing \"" + decodeURI(target.dataset.key) + "\" with " + target.id;
-      chatbox.innerHTML = item.msg;
-      chatHistory.appendChild(chatbox);
-    });
-  }
 };
 
 var _executeCommand = function(target) {
@@ -619,7 +561,7 @@ var initUI = function() {
 var init = function() {
   initVerbsMapping();
   initUI();
-  DialogManager.init();
+  DialogManager.init(chatHistory);
 };
 
 localforage.getItem(stoerKey, function(err, value) {
