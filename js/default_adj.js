9b8d4b9823c0074aeda48cb0ce481d74030a1ac4
gasolin <gasolin@gmail.com>
2016-01-24 00:25:45 +0800

    feat(verbopen): add IFTTT as new verbOpen type option
    #74

--- js/default_adj.js
+++ js/default_adj.js
@@ -4,6 +4,8 @@
 var defaultAdjStore = {
   showWidget: '<iframe src="{url}" height="320" width="480" ' +
     'frameBorder="0"></iframe>',
+  showWebhook: '{name} pinged',
+  showWebhookFailed: '{name} failed: {msg}',
   showLink: 'Here you are: <a href="{url}" target="_blank">{provider}</a>',
   actionSearch: '{verb} "{searchTerms}" with {provider}',
   actionSearchReply: 'I am {verb}ing "{searchTerms}" with {provider}...<br/>' +
